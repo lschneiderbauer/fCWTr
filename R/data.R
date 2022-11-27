2152a2c3fe7d9fb713567098f18a6425572a834d
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2022-11-27 18:05:49 +0100

    update example data

--- R/data.R
+++ R/data.R
@@ -1,6 +1,13 @@
 #' Superimposed sin waves of different frequencies and different amplitudes
 #' (440 Hz, 880 Hz, 100 Hz, 500 Hz, 1200 Hz, 50 Hz, assuming a sample rate of 44100 Hz)
 #'
-#' @format ## `time_series_sin`
+#' @format ## `ts_sin_superpos`
 #' A vector of float containing a signal time series.
-"time_series_sin"
+"ts_sin_superpos"
+
+#' Pure (long) sin wave of 440 Hz
+#' assuming a sample rate of 44100 Hz)
+#'
+#' @format ## `ts_sin_440`
+#' A vector of float containing a signal time series.
+"ts_sin_440"
--- data-raw/ts_sin.R
+++ data-raw/ts_sin.R
@@ -6,7 +6,7 @@ sampling_rate <- 44100
 # a bunch of superimposed sin waves
 # of 440, 880, 100, 500, 1200 and 50 Hz
 # assuming a sampling_rate of 44100
-time_series_sin <-
+ts_sin_superpos <-
   c(
     sin((1:3000) * 2 * pi * 440 / sampling_rate) +
       sin((1:3000) * 2 * pi * 880 / sampling_rate) +
@@ -16,4 +16,14 @@ time_series_sin <-
       sin((1:3000) * 2 * pi * 50 / sampling_rate)
   )
 
-usethis::use_data(time_series_sin, overwrite = TRUE)
+# 1 sec sin signal
+ts_sin_440 <- sin((1:44100) * 2 * pi * 440 / sampling_rate)
+
+
+
+# 10 sec sin signal with sin-varying frequency
+f <- \(t,a) { 1.01*t + (1-cos(a*t))/a }
+ts_sin_sin <- sin(2 * pi * 5000 / sampling_rate * f((1:441000), 4*pi / 441000))
+
+usethis::use_data(ts_sin_superpos, ts_sin_440, ts_sin_sin, overwrite = TRUE)
+
