5ef8cc6ca7ccee3c6c2f6afa1940ab877a27ee00
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2024-05-20 18:47:10 +0200

    add more tests

--- tests/testthat/test-fcwt.R
+++ tests/testthat/test-fcwt.R
@@ -1,4 +1,4 @@
-test_that("fctw() returns a vector of expected length", {
+test_that("fcwt() returns a vector of expected length", {
   expect_length(
     fcwt(
       ts_sin_440,
@@ -24,7 +24,7 @@ test_that("fctw() returns a vector of expected length", {
   )
 })
 
-test_that("fctw() errs if frequency specs are higher than Nyquist frequency", {
+test_that("fcwt() errs if frequency specs are higher than Nyquist frequency", {
   expect_error(
     fcwt(
       ts_sin_440,
@@ -34,5 +34,19 @@ test_that("fctw() errs if frequency specs are higher than Nyquist frequency", {
       n_freqs = 10,
       sigma = 1
     )
+  )
+})
+
+test_that("fcwt() returns same result", {
+  expect_snapshot(
+    fcwt(
+      ts_sin_440,
+      sample_freq = 44100,
+      freq_begin = 50,
+      freq_end = 1000,
+      n_freqs = 10,
+      sigma = 1
+    ) |>
+      agg(1000)
   )
 })
--- tests/testthat/test-fcwt_batch.R
+++ tests/testthat/test-fcwt_batch.R
@@ -27,32 +27,33 @@ test_that("fcwt batching yields identical result in single-batch case", {
   )
 })
 
-# test_that("fcwt batching", {
-#   res0 <-
-#     fcwt(
-#       ts_sin_sin,
-#       sample_freq = 44100,
-#       freq_begin = 30,
-#       freq_end = 11000,
-#       n_freqs = 300,
-#       sigma = 10,
-#     ) |>
-#     rm_na_time_slices()
-#
-#   res_batch <-
-#     fcwt_batch(
-#       ts_sin_sin,
-#       sample_freq = 44100,
-#       freq_begin = 30,
-#       freq_end = 11000,
-#       n_freqs = 300,
-#       sigma = 10,
-#       max_batch_size = 10000,
-#       time_resolution = 1 / 44100
-#     )
-#
-#   expect_equal(
-#     res0,
-#     res_batch
-#   )
-# })
+test_that("fcwt batching is roughly equal to non-batching", {
+  res0 <-
+    fcwt(
+      ts_sin_sin,
+      sample_freq = 44100,
+      freq_begin = 100,
+      freq_end = 11000,
+      n_freqs = 300,
+      sigma = 10,
+    ) |>
+    rm_na_time_slices()
+
+  res_batch <-
+    fcwt_batch(
+      ts_sin_sin,
+      sample_freq = 44100,
+      freq_begin = 100,
+      freq_end = 11000,
+      n_freqs = 300,
+      sigma = 10,
+      max_batch_size = 20000,
+      time_resolution = 1 / 44100
+    )
+
+  expect_equal(
+    agg(res0, n = 100),
+    agg(res_batch, n = 100),
+    tolerance = 10^-2
+  )
+})
