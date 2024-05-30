9e220a29d7b446352b6fd386a4d6296947cda2a7
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2024-05-30 11:11:52 +0200

    simplify snapshot test

--- tests/testthat/test-fcwt.R
+++ tests/testthat/test-fcwt.R
@@ -37,24 +37,25 @@ test_that("fcwt() errs if frequency specs are higher than Nyquist frequency", {
   )
 })
 
-test_that("fcwt() returns same result", {
-  res_vec <-
-    round(
-      c(
-        fcwt(
-          ts_sin_440,
-          sample_freq = 44100,
-          freq_begin = 50,
-          freq_end = 1000,
-          n_freqs = 10,
-          sigma = 1
-        ) |>
-          agg(1000)
-      ),
-      digits = 5
+test_that("fcwt() result does not change", {
+  res <-
+    as.data.frame(
+      fcwt(
+        ts_sin_440,
+        sample_freq = 44100,
+        freq_begin = 50,
+        freq_end = 1000,
+        n_freqs = 5,
+        sigma = 1
+      ) |>
+        agg(10)
     )
 
-  expect_snapshot(
-    res_vec
+  values <- res$value[order(res$time, res$freq)]
+
+  expect_snapshot_value(
+    values,
+    tolerance = 10^-5,
+    style = "json2"
   )
 })
