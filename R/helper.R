74514bc1cb856727332b25b8c8cfe0bcbeff27f1
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2024-06-09 19:52:27 +0200

    remove obsolete helper functions

--- R/helper.R
+++
@@ -1,8 +0,0 @@
-# logarithmic spaced sequence
-lseq <- function(from, to, length.out) {
-  2^(seq(log2(from), log2(to), length.out = length.out))
-}
-
-freqs_to_noctaves <- function(min_freq, max_freq) {
-  ceiling(log2(max_freq / min_freq))
-}
