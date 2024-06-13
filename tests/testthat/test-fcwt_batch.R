9cf67506bb32f62d93f30ecd24a6eb0eed130d8a
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2024-06-13 20:20:00 +0200

    remove long running test

--- tests/testthat/test-fcwt_batch.R
+++ tests/testthat/test-fcwt_batch.R
@@ -26,34 +26,3 @@ test_that("fcwt batching yields identical result in single-batch case", {
     res_batch
   )
 })
-
-test_that("fcwt batching is roughly equal to non-batching", {
-  res0 <-
-    fcwt(
-      ts_sin_sin,
-      sample_freq = 44100,
-      freq_begin = 100,
-      freq_end = 11000,
-      n_freqs = 300,
-      sigma = 10,
-    ) |>
-    rm_na_time_slices()
-
-  res_batch <-
-    fcwt_batch(
-      ts_sin_sin,
-      sample_freq = 44100,
-      freq_begin = 100,
-      freq_end = 11000,
-      n_freqs = 300,
-      sigma = 10,
-      max_batch_size = 20000,
-      time_resolution = 1 / 44100
-    )
-
-  expect_equal(
-    agg(res0, n = 100),
-    agg(res_batch, n = 100),
-    tolerance = 10^-2
-  )
-})
