cadbc0362dcb25204124900a37d7e63cf952f273
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2024-06-15 12:50:52 +0200

    add even more return value documentation

--- R/fcwt_batch.R
+++ R/fcwt_batch.R
@@ -48,6 +48,8 @@
 #'  The spectogram, a numeric real-valued matrix with dimensions roughly
 #'  `dim ~ c(length(signal) * time_resolution * sample_freq, n_freqs)`.
 #'  The exact length of the output depends on boundary effect details.
+#'  This matrix is wrapped into a S3-class `fcwtr_scalogram` so that plotting and
+#'  coercion functions can be used conveniently.
 #'
 #' @inheritParams fcwt
 #' @seealso [fcwt()]
