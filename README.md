b0e6fa6437196b3efc808556f6252bd715695129
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2024-12-29 11:38:51 +0100

    add hex logo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# fCWTr
+# fCWTr <a href="https://lschneiderbauer.github.io/fcwtr/"><img src="man/figures/logo.png" align="right" height="139" alt="fCWTr website" /></a>
 
 <!-- badges: start -->
 
---
+++ data-raw/hex-logo.R
@@ -0,0 +1,47 @@
+library(hexSticker)
+library(tidyverse)
+library(showtext)
+
+## Loading Google fonts (http://www.google.com/fonts)
+font_add_google("Tiny5", db_cache = FALSE)
+font_add_google("Micro 5", db_cache = FALSE)
+
+
+morlet <- function(x, sigma) {
+  exp(-x^2) * sin(2 * pi * x * sigma)
+}
+
+p <-
+  tibble(x = seq(-2.5, 2.5, by = 0.025)) |>
+  mutate(
+    y = morlet(x, 1),
+    c = abs(x)
+  ) |>
+  ggplot(aes(x = x, y = y)) +
+  # geom_line(linewidth = 1.5) +
+  geom_point(size = 0.8, shape = "square") +
+  # scale_color_viridis() +
+  theme_void() +
+  theme(
+    legend.position = "none"
+  )
+p
+
+# hexSticker conventions (by experiment)
+# (x,y) = (1,1) seems to be the center of hex
+# (x,y) = (0,0) is left bottom corner
+
+sticker(
+  p,
+  package = "",
+  s_x = 1, s_y = 1,
+  s_width = 2, s_height = 1.6,
+  h_fill = "white",
+  h_color = "black",
+  url = "fCWTr",
+  u_size = 26,
+  u_family = "Micro 5",
+  u_x = 1.15,
+  u_y = 0.23,
+  filename = "inst/figures/hex-logo.png"
+) |> plot()
