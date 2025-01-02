bc6135b3a22d75846fe6d1b194fa310ac50fcf71
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2025-01-02 19:23:28 +0100

    shortened sigma function names and add another helper

--- R/sigma.R
+++ R/sigma.R
@@ -19,12 +19,12 @@
 #'  units. They obey the property \eqn{time \times freq = \frac{8]{\pi}}}.
 #'
 #' @examples
-#' sigma_resolution(1, u(440, "Hz"))
+#' sigma_res(1, u(440, "Hz"))
 #'
 #' @concept sigma
 #' @family sigma
 #' @export
-sigma_resolution <- function(sigma, freq) {
+sigma_res <- function(sigma, freq) {
   stopifnot(is.numeric(sigma), sigma > 0)
   stopifnot(inherits(freq, "units"), has_comp_unit(freq, "Hz"))
 
@@ -46,7 +46,7 @@ sigma_resolution <- function(sigma, freq) {
 #' balance for a particular use case can be crucial. This function determines
 #' the value of sigma for a particular frequency resolution requirement.
 #'
-#' @param resolution
+#' @param res
 #'  The required frequency resolution in frequency units (see [u()]).
 #'
 #' @param freq
@@ -59,20 +59,20 @@ sigma_resolution <- function(sigma, freq) {
 #'
 #' @examples
 #' ref_freq <- u(440, "Hz")
-#' sigma <- sigma_from_frequency_resolution(u(7, "Hz"), ref_freq)
+#' sigma <- sigma_from_freq_res(u(7, "Hz"), ref_freq)
 #'
-#' sigma_resolution(sigma, ref_freq)
+#' sigma_res(sigma, ref_freq)
 #'
 #' @concept sigma
 #' @family sigma
 #' @export
-sigma_from_frequency_resolution <- function(resolution, freq) {
-  stopifnot(inherits(resolution, "units"), has_comp_unit(resolution, "Hz"))
+sigma_from_freq_res <- function(res, freq) {
+  stopifnot(inherits(res, "units"), has_comp_unit(res, "Hz"))
   stopifnot(inherits(freq, "units"), has_comp_unit(freq, "Hz"))
-  stopifnot(resolution > u(0, "Hz"))
+  stopifnot(res > u(0, "Hz"))
   stopifnot(freq > u(0, "Hz"))
 
-  ddu(4 / (2 * pi) * freq / resolution)
+  ddu(4 / (2 * pi) * freq / res)
 }
 
 #' Determine Sigma from a time resolution requirement
@@ -87,7 +87,7 @@ sigma_from_frequency_resolution <- function(resolution, freq) {
 #' balance for a particular use case can be crucial. This function determines
 #' the value of sigma for a particular time resolution requirement.
 #'
-#' @param resolution
+#' @param res
 #'  The required time resolution in frequency units (see [u()]).
 #'
 #' @param freq
@@ -100,18 +100,53 @@ sigma_from_frequency_resolution <- function(resolution, freq) {
 #'
 #' @examples
 #' ref_freq <- u(440, "Hz")
-#' sigma <- sigma_from_time_resolution(u(10, "ms"), ref_freq)
+#' sigma <- sigma_from_time_res(u(10, "ms"), ref_freq)
 #'
-#' sigma_resolution(sigma, ref_freq)
+#' sigma_res(sigma, ref_freq)
 #'
 #' @concept sigma
 #' @family sigma
 #' @export
-sigma_from_time_resolution <- function(resolution, freq) {
-  stopifnot(inherits(resolution, "units"), has_comp_unit(resolution, "s"))
+sigma_from_time_res <- function(res, freq) {
+  stopifnot(inherits(res, "units"), has_comp_unit(res, "s"))
   stopifnot(inherits(freq, "units"), has_comp_unit(freq, "Hz"))
-  stopifnot(resolution > u(0, "s"))
+  stopifnot(res > u(0, "s"))
   stopifnot(freq > u(0, "Hz"))
 
-  ddu(freq * resolution / 4)
+  ddu(freq * res / 4)
+}
+
+#' Determine Sigma from a relative frequency resolution requirement
+#'
+#' Setting the correct value for `sigma` in [fcwt()] for a particular
+#' use case is important.
+#' Sigma controls the wavelet spread used by the FCWT and so determines
+#' the time-/frequency resolution (depending on a given reference frequency).
+#'
+#' If you set a high frequency resolution, the time resolution
+#' of the output signal will suffer, and vice versa. Determining the correct
+#' balance for a particular use case can be crucial. This function determines
+#' the value of sigma for a particular relative frequency resolution requirement.
+#' Note that the relative frequency resolution does not depend on the frequency
+#' itself, in contrast to the absolute frequency resolution.
+#'
+#' @param res
+#'  The required relative frequency resolution, a number between 0 and 1.
+#'
+#' @return
+#'  A dimensionless number controlling the FCWT wavelet spread,
+#'  corresponding to the `sigma` argument in [fcwt()].
+#'
+#' @examples
+#' sigma <- sigma_from_freq_res_rel(0.2)
+#'
+#' sigma_res(sigma, ref_freq)
+#'
+#' @concept sigma
+#' @family sigma
+#' @export
+sigma_from_freq_res_rel <- function(res) {
+  stopifnot(res > 0, res < 1)
+
+  2 / (pi * res)
 }
--- tests/testthat/test-sigma.R
+++ tests/testthat/test-sigma.R
@@ -1,6 +1,6 @@
 test_that("uncertinaty principle", {
-  res <- sigma_resolution(10, u(20, "Hz"))
-  res2 <- sigma_resolution(1, u(30, "Hz"))
+  res <- sigma_res(10, u(20, "Hz"))
+  res2 <- sigma_res(1, u(30, "Hz"))
 
   expect_equal(du(res$time * res$freq), 8/( pi))
   expect_equal(du(res2$time * res2$freq), 8/(pi))
@@ -8,18 +8,26 @@ test_that("uncertinaty principle", {
 
 test_that("sigma calculator", {
   expect_equal(
-    sigma_from_frequency_resolution(u(1, "Hz"), u(10, "Hz")),
+    sigma_from_freq_res(u(1, "Hz"), u(10, "Hz")),
     6.366198,
     tolerance = 10^-4
   )
   expect_equal(
-    sigma_from_time_resolution(u(1, "s"), u(10, "Hz")),
-    2.5
+    sigma_from_time_res(u(1, "s"), u(10, "Hz")),
+    2.5,
+    tolerance = 10^-4
+  )
+
+  expect_equal(
+    sigma_from_freq_res_rel(0.1),
+    6.366198,
+    tolerance = 10^-4
   )
 })
 
 test_that("sigma errs", {
-  expect_error(sigma_resolution(-10, u(10, "Hz")))
-  expect_error(sigma_from_frequency_resolution(-u(10, "Hz"), u(10, "Hz")))
-  expect_error(sigma_from_time_resolution(-u(10, "s"), u(10, "Hz")))
+  expect_error(sigma_res(-10, u(10, "Hz")))
+  expect_error(sigma_from_freq_res(-u(10, "Hz"), u(10, "Hz")))
+  expect_error(sigma_from_time_res(-u(10, "s"), u(10, "Hz")))
+  expect_error(sigma_from_freq_res_rel(-1))
 })
